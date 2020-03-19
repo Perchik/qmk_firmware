26f9ff011b61a275ac74eaf84021d0e587eb6397
Perchik <pdukes3@gmail.com>
2020-03-19 16:03:14 -0400

    my tweaks

--- keyboards/ergodox_infinity/keymaps/pdukes/visualizer.c
+++ keyboards/ergodox_infinity/keymaps/pdukes/visualizer.c
@@ -17,7 +17,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "simple_visualizer.h"
 #include "util.h"
-
+#define L_BASE 0 /* Default Layer. */
+#define L_SHRT 1 /* Shortcuts */
+#define L_MDIA 2/* Numpad, Media & Mouse Keys. */
+#define L_NUM 3 /* Numbers. */
+#define L_FN 4 /* FN. */
 // This function should be implemented by the keymap visualizer
 // Don't change anything else than state->target_lcd_color and state->layer_text as that's the only thing
 // that the simple_visualizer assumes that you are updating
@@ -27,20 +31,24 @@ static void get_visualizer_layer_and_color(visualizer_state_t* state) {
     uint8_t layer = biton32(state->status.layer);
 
     switch (layer) {
-        case 5:
+        case L_MDIA:
             // green
             state->target_lcd_color = LCD_COLOR(85, 255, 128);
             state->layer_text       = "Mouse, Numpad";
             break;
-        case 4:
+        case L_NUM:
             // orange
             state->target_lcd_color = LCD_COLOR(28, 255, 230);
             state->layer_text       = "Home - numbers";
             break;
-        case 7:
+        case L_FN:
             // blue
             state->target_lcd_color = LCD_COLOR(149, 255, 192);
             state->layer_text       = "Fn v1.2";
+            break;
+        case L_SHRT:
+            state->target_lcd_color = LCD_COLOR(60,100,100);
+            state->layer_text = " Shortcuts";
             break;
         default:
             // orange
